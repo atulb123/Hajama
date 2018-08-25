eb3a76080660a6c958112a005f1d5bbe301a9545
atul123 <abharadwajj@gmail.com>
2018-08-25 19:34:36 +0530

    test1

--- src/main/java/B.java
+++ src/main/java/B.java
@@ -3,10 +3,9 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("test"); 
-<<<<<<< HEAD
+
 		System.out.println("eeee1e"); 
-=======
->>>>>>> e67f546b004436d0806bce10636425f9b68a16eb
+
 		
 	}
 
